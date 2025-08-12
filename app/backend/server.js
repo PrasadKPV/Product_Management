2d1ee0dd656e247a5175e26f60ab4cf6257b735c
PrasadKPV <kpvprasad2000@gmail.com>
2025-08-12 18:14:29 +0530

    fix

---
+++ app/backend/server.js
@@ -0,0 +1,23 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const productRoutes = require('./routes/products');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+app.use('/api/products', productRoutes);
+
+if (!process.env.VERCEL) {
+  app.listen(5000, () => console.log('Server started on port 5000'));
+}
+
+module.exports = app;
